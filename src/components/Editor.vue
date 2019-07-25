f93ef11aa3f2c43fc89b246acd641e29ea0e3a04
Ryan Wang <hwwangwang@gmail.com>
2019-07-25 12:01:31 +0800

    add undo and redo

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -383,6 +383,43 @@
             <path d="M0 0h24v24H0z" fill="none" />
           </svg>
         </button>
+        <button
+          v-tooltip.bottom="'放弃'"
+          class="menubar__button"
+          @click="commands.undo"
+        >
+          <svg
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M12.5 8c-2.65 0-5.05.99-6.9 2.6L2 7v9h9l-3.62-3.62c1.39-1.16 3.16-1.88 5.12-1.88 3.54 0 6.55 2.31 7.6 5.5l2.37-.78C21.08 11.03 17.15 8 12.5 8z"
+            />
+          </svg>
+        </button>
+
+        <button
+          v-tooltip.bottom="'重做'"
+          class="menubar__button"
+          @click="commands.redo"
+        >
+          <svg
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M18.4 10.6C16.55 8.99 14.15 8 11.5 8c-4.65 0-8.58 3.03-9.96 7.22L3.9 16c1.05-3.19 4.05-5.5 7.6-5.5 1.95 0 3.73.72 5.12 1.88L13 16h9V7l-3.6 3.6z"
+            />
+          </svg>
+        </button>
       </div>
     </editor-menu-bar>
     <editor-content
@@ -442,7 +479,7 @@ export default {
     },
     width: {
       required: false,
-      default: '700px',
+      default: '760px',
       type: String,
     },
     imageProvider: {
